bbdcb1d15ddefd726f4a050d07ac6673b776ede0
Cleidson Amorim <116361667+cleiamorim@users.noreply.github.com>
2026-07-08 14:02:57 -0300

    Update database schema and data for inf07sst

--- DB/database.sql
+++ DB/database.sql
@@ -33,16 +33,18 @@ CREATE TABLE `dimfuncionarios` (
   `whatsapp` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id_funcionario`),
   UNIQUE KEY `cpf` (`cpf`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `dimfuncionarios`
 --
 
+LOCK TABLES `dimfuncionarios` WRITE;
 /*!40000 ALTER TABLE `dimfuncionarios` DISABLE KEYS */;
-INSERT INTO `dimfuncionarios` VALUES (12,'Ricardo Amorim','123.416.789-00','Consultor dados','DBA',NULL,NULL,NULL),(13,'Cleidson Amorim','000.000.111-00','Especialista em dados Fullstack','Data Science',NULL,NULL,NULL);
+INSERT INTO `dimfuncionarios` VALUES (2,'Maria Oliveira','987.654.321-11','Eletricista de Manutenção','Produção',NULL,NULL,NULL),(3,'Carlos Souza','456.789.123-22','Supervisor de Almoxarifado','Logística',NULL,NULL,NULL),(7,'Nadia Cristina Xavier','012.857.478-00','Administradora','CEO','nadiacxavier@gmail.com','3198745610\'','3198745610'),(9,'CLEIDSON VICENTE DE AMORIM','07600163640','Data Analyst','Dados','profcleiamorim@gmail.com','31992975647','31992975647');
 /*!40000 ALTER TABLE `dimfuncionarios` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `dimtreinamentos`
@@ -63,9 +65,11 @@ CREATE TABLE `dimtreinamentos` (
 -- Dumping data for table `dimtreinamentos`
 --
 
+LOCK TABLES `dimtreinamentos` WRITE;
 /*!40000 ALTER TABLE `dimtreinamentos` DISABLE KEYS */;
-INSERT INTO `dimtreinamentos` VALUES (6,'NR-35 Trabalho em Altura',36),(7,'NR-06 - Treinamento de EPIs',12);
+INSERT INTO `dimtreinamentos` VALUES (1,'NR 11 - Transporte e Movimentação de Materiais',12),(2,'NR 10 - Segurança em Instalações Elétricas',24),(3,'NR 35 - Trabalho em Altura',24),(5,'NR 33 - Espaço confinado',24),(6,'PAEBM - (Plano de Ação de Emergência para Barragens de Mineração)',6),(7,'NR 06 - Equipamento de Proteção Individual',36);
 /*!40000 ALTER TABLE `dimtreinamentos` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `factregistros`
@@ -79,27 +83,25 @@ CREATE TABLE `factregistros` (
   `id_funcionario` int NOT NULL,
   `id_treinamento` int NOT NULL,
   `data_realizacao` date NOT NULL,
+  `data_vencimento` date NOT NULL,
   `status` varchar(20) DEFAULT 'Ativo',
-  `data_vencimento` date DEFAULT NULL,
   PRIMARY KEY (`id_registro`),
   KEY `id_funcionario` (`id_funcionario`),
   KEY `id_treinamento` (`id_treinamento`),
   CONSTRAINT `factregistros_ibfk_1` FOREIGN KEY (`id_funcionario`) REFERENCES `dimfuncionarios` (`id_funcionario`) ON DELETE CASCADE,
   CONSTRAINT `factregistros_ibfk_2` FOREIGN KEY (`id_treinamento`) REFERENCES `dimtreinamentos` (`id_treinamento`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `factregistros`
 --
 
+LOCK TABLES `factregistros` WRITE;
 /*!40000 ALTER TABLE `factregistros` DISABLE KEYS */;
-INSERT INTO `factregistros` VALUES (1,13,6,'2026-06-11','Ativo','2027-06-11'),(5,12,6,'2026-06-11','Ativo','2027-06-11'),(7,12,6,'2026-06-11','Concluído','2027-06-11');
+INSERT INTO `factregistros` VALUES (21,7,1,'2026-07-07','2027-07-07','Ativo'),(22,9,2,'2026-07-07','2028-07-07','Ativo'),(23,2,3,'2026-07-07','2028-07-07','Ativo'),(24,2,6,'2025-01-01','2025-07-01','Ativo'),(25,9,7,'2025-03-14','2028-03-14','Ativo'),(26,9,5,'2024-07-14','2026-07-14','Ativo');
 /*!40000 ALTER TABLE `factregistros` ENABLE KEYS */;
-
---
--- Dumping routines for database 'inf07sst'
---
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -110,4 +112,4 @@ INSERT INTO `factregistros` VALUES (1,13,6,'2026-06-11','Ativo','2027-06-11'),(5
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-11 17:48:30
+-- Dump completed on 2026-07-07 14:18:22
